eb02fe242a2a988f806c1313a636cfde61acd75f
YubaC <49724342+YubaC@users.noreply.github.com>
2023-02-25 18:07:38 +0800

    Add reload-settings.js

---
+++ assets/js/reload-settings.js
@@ -0,0 +1,19 @@
+"use strict";
+// !Works when settings were changed in the settings page or new content div or iframe was added
+
+/**
+ * Reloads the settings, this is called when the settings are changed in the settings page or when a new content div or iframe is added.
+ */
+function reloadSettings() {
+  var $itemsToUpdate = $(
+    "." + contentBoxClass + ", ." + contentBoxClass + "Active"
+  );
+
+  //   !Settings to be updated
+  //   * font-size
+  $itemsToUpdate.css(
+    "font-size",
+    settings["font-size"] * 20 + 100 + "%",
+    " !important"
+  );
+}
